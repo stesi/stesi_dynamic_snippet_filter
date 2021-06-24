63c6ffcdfe5228dc7dfe92a76bb336c3a4cde7a5
Francesco Pranzo <pranzo.f@stesi.eu>
2021-06-24 08:58:22 +0200

    update

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,7 +4,15 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    show_in_website = fields.Boolean("Show in website?")
-    # selected_partner_id = fields.Many2one('stesi.res.partner', string="Partner on Website")
     description = fields.Html()
     website_image = fields.Binary("Website Image")
+    web_image = fields.Binary("Web Image", compute="_compute_web_image")
+
+    def _compute_web_image(self):
+        for partner in self:
+            if partner.website_image:
+                partner.web_image = partner.website_image
+            elif partner.image_1920:
+                partner.web_image = partner.image_1920
+            else:
+                partner.web_image = False
